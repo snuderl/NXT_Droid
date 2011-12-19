2a105d44ec6f8486863a432b39dc3cc70bbefe4c
Snuderl <Snuderl@gmail.com>
2011-12-19 19:47:58 +0100

    Ustvarjen project

---
+++ src/org/nxt/droid/NXT_DroidActivity.java
@@ -0,0 +1,13 @@
+package org.nxt.droid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NXT_DroidActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
