1a5dfc69a33839c26d517c9fa29f21b3b8485fb0
Snuderl <Snuderl@gmail.com>
2011-12-23 12:36:06 +0100

    Connected to sensor

--- src/org/nxt/droid/ControlActivity.java
+++ src/org/nxt/droid/ControlActivity.java
@@ -5,6 +5,10 @@ package org.nxt.droid;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothSocket;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -12,42 +16,97 @@ import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * @author Blaž Šnuderl
- *
+ * 
  */
-public class ControlActivity extends Activity {
+public class ControlActivity extends Activity implements SensorEventListener {
 
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.control);
-		
-		BluetoothSocket bs = ((Application)getApplicationContext()).bs;
+
+		BluetoothSocket bs = ((Application) getApplicationContext()).bs;
 		UiMessage messageHandler = new UiMessage();
+		sManager = (SensorManager) getSystemService(SENSOR_SERVICE); 
+
+		tv = (TextView) findViewById(R.id.textView1);
 		
-		final Controls control = new Controls(messageHandler,bs);
+		control = new Controls(messageHandler, bs);
+		
 		control.run();
 
-		Button sendButton = (Button)findViewById(R.id.send_button);
+		Button sendButton = (Button) findViewById(R.id.send_button);
 		sendButton.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				control.send(1, 1);
-				
+
 			}
 		});
 
-		
 	}
-	class UiMessage extends Handler{
+
+	class UiMessage extends Handler {
 		float[] pos;
 
 		@Override
 		public void handleMessage(Message msg) {
-			Log.d("Message recieved", msg.arg1 + ","+msg.arg2 + ".");
+			Log.d("Message recieved", msg.arg1 + "," + msg.arg2 + ".");
 		}
 	}
+
+	// When this Activity isn't visible anymore
+	@Override
+	protected void onStop() {
+		// unregister the sensor listener
+		sManager.unregisterListener(this);
+		super.onStop();
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor arg0, int arg1) {
+		// Do nothing
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		// if sensor is unreliable, return void
+		if (event.accuracy == SensorManager.SENSOR_STATUS_UNRELIABLE) {
+			return;
+		}
+		
+		tv.setText("Os X :"+ Float.toString(event.values[2]) +"\n"+
+				"Os Y :"+ Float.toString(event.values[1]) +"\n"+
+				"Os Z :"+ Float.toString(event.values[0]));
+		
+		control.send(4, event.values[0]);
+		
+		
+	}
+
+	// when this Activity starts
+	@Override
+	protected void onResume() {
+		super.onResume();
+		/*
+		 * register the sensor listener to listen to the gyroscope sensor, use
+		 * the callbacks defined in this class, and gather the sensor
+		 * information as quick as possible
+		 */
+		sManager.registerListener(this,
+				sManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
+				SensorManager.SENSOR_DELAY_GAME);
+	}
+
+	// a TextView
+	private TextView tv;
+	// the Sensor Manager
+	private SensorManager sManager;
+	
+	Controls control;
 }
---
+++ src/org/nxt/droid/GyroListener.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package org.nxt.droid;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+
+/**
+ * @author Blaž Šnuderl
+ *
+ */
+public class GyroListener implements SensorEventListener{
+
+	/* (non-Javadoc)
+	 * @see android.hardware.SensorEventListener#onAccuracyChanged(android.hardware.Sensor, int)
+	 */
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see android.hardware.SensorEventListener#onSensorChanged(android.hardware.SensorEvent)
+	 */
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
